fbc0d874880d0e74cf65572eec36e341b3195299
joannawritescode <joannawritescode@gmail.com>
2025-04-13 13:44:50 -0500

    4-13-25

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var selected;
+
 $(document).ready(function(){
     
     // create the board
@@ -21,17 +23,58 @@ $(document).ready(function(){
     }
 
     $('.piece').on("click", function(){
-        $('.selected').removeClass("selected");
-        $(this).addClass("selected");
-    });
-
-    $('.square.red').on("click", function(){
-        if ( $('.selected').length > 0 ) {
-            $(this).append( $('.selected') );
-            // $('.selected').removeClass("selected");
-            // cannot put multiple pieces into a square
+        // check whose turn it is
+        let turn = $('#turn').html();
+        if ( $(this).hasClass( turn ) ) {
+            $('.selected').removeClass("selected");
+            selected = $(this);
+            moveChecker();
+            $(this).addClass("selected");
         }
     });
 
+    // click for red squares that are EMPTY
+    // cannot put multiple pieces into a square
+    $('.square.red').on("click", function(){
+        if ( $('.selected').length > 0 && $(this).children().length == 0 ) {
+            $(this).append( $('.selected') );
+            $('.selected').removeClass("selected");
+            changeTurn();
+        }
+    });
 
 })
+
+function changeTurn() {
+    let turn = $('#turn').html();
+    if ( turn == "black" ) {
+        $('#turn').html("white");
+    } else if ( turn == "white" ) {
+        $('#turn').html("black");
+    } else {
+        $('#turn').html("HUMAN ERROR.");
+    }
+}
+
+function moveChecker() {
+    // does this piece have squares it can move to?
+    /*
+        if yes, highlight!
+        if no, do nothing.
+    */
+    let prow = parseInt(selected.parent().attr("id")[0]);
+    let pcol = parseInt(selected.parent().attr("id")[1]);
+    
+    // 1 row down 1 col left
+    $(`#${prow + 1}${pcol - 1}`).addClass('selected');
+
+    // 1 row down 1 col right
+    $(`#${prow + 1}${pcol + 1}`).addClass('selected');
+    
+}
+
+/* IDK
+
+//&& $(this) != $('.selected').parent('.square')
+ 
+*/
